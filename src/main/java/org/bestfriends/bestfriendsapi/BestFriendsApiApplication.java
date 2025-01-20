f27d2c92d05ea46ea006f86a3b9582690f21dae5
WillyLS <WLScripps@gmail.com>
2025-01-20 01:48:12 -0700

    Small clean up

--- src/main/java/org/bestfriends/bestfriendsapi/BestFriendsApiApplication.java
+++ src/main/java/org/bestfriends/bestfriendsapi/BestFriendsApiApplication.java
@@ -1,27 +1,26 @@
 package org.bestfriends.bestfriendsapi;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import com.toedter.spring.hateoas.jsonapi.JsonApiConfiguration;
 import com.toedter.spring.hateoas.jsonapi.JsonApiObject;
 
-@SpringBootApplication(exclude = GsonAutoConfiguration.class)
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
 public class BestFriendsApiApplication {
-  private static final Logger logger = LoggerFactory.getLogger(BestFriendsApiApplication.class);
 
   public static void main(String[] args) {
-    logger.debug("Starting Application");
+    log.debug("Starting Application");
     SpringApplication.run(BestFriendsApiApplication.class, args);
   }
 
   @Bean
   JsonApiConfiguration jsonApiConfiguration() {
-    return new JsonApiConfiguration().withJsonApiObject(new JsonApiObject(true));
+    return new JsonApiConfiguration().withJsonApiObject(new JsonApiObject(true)).withPluralizedTypeRendered(false);
   }
 
 }
