c9a84d80017551f57b149f945a4c09c3177961e7
kmdunn5 <kmdunn5@gmail.com>
2025-01-19 22:23:18 -0500

    adding h2 db interface

--- src/main/java/org/bestfriends/bestfriendsapi/ti4/contexts/models/UnitDAO.java
+++ src/main/java/org/bestfriends/bestfriendsapi/ti4/contexts/models/UnitDAO.java
@@ -1,5 +1,13 @@
 package org.bestfriends.bestfriendsapi.ti4.contexts.models;
 
-public class UnitDAO {
+import jakarta.persistence.*;
 
+@Entity
+public class UnitDAO {
+  @Id
+  @GeneratedValue
+  private Long id;
+  
+  @Column
+  private String name;
 }
