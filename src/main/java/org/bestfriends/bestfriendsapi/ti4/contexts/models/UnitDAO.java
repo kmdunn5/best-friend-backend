70b209c7696421924d69ae6e81be0c88ce50bf63
Ken Dunn <kmdunn5@gmail.com>
2026-03-13 14:42:34 -0400

    using an actual database with easy migrations via flyway and an explanation of how to use it.

--- src/main/java/org/bestfriends/bestfriendsapi/ti4/contexts/models/UnitDAO.java
+++ src/main/java/org/bestfriends/bestfriendsapi/ti4/contexts/models/UnitDAO.java
@@ -3,9 +3,10 @@ package org.bestfriends.bestfriendsapi.ti4.contexts.models;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "unit_dao")
 public class UnitDAO {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   
   @Column
